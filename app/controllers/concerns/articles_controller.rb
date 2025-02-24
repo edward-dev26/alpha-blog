399ae4e023c415115b6ff0908b02aaa61786055f
Edward <eshvetsov68@gmail.com>
2025-02-24 12:24:42 +0100

    Add articles create/update pages

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -6,4 +6,36 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    data = params.require(:article).permit(:title, :description)
+    @article = Article.new(data)
+
+    if @article.save
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    data = params.require(:article).permit(:title, :description)
+    @article = Article.find(params[:id])
+
+    if @article.update(data)
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
