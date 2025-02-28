519c30f6e24d595413baabc0e20b757e0fb96397
Edward <eshvetsov68@gmail.com>
2025-02-28 18:18:59 +0100

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :username,
+            presence: true,
+            length: { minimum: 3, maximum: 25 },
+            uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\z/ }
+end
