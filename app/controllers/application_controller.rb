efb2dc1d9e8e2416ef5e144dddfff8b37e094ca4
Edward <eshvetsov68@gmail.com>
2025-03-10 11:44:54 +0100

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :is_auth
+
+  def current_user
+    user_id = session[:user_id]
+    return nil if user_id.nil?
+
+    @current_user ||= User.find(user_id)
+  end
+
+  def is_auth
+    !!current_user
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    redirect_to articles_path if is_auth
   end
 
   def about
